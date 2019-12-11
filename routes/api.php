de9c8ae66623c1bca4b2dda3fa0885b345e16473
rachelroll <hai30@163.com>
2019-12-11 23:52:28 +0800

    update(passport)

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->prefix('v1')->group(function () {
+    //注册用户
+    Route::post('/auth/register', 'AuthController@register')->name('api.auth.register');
+    Route::post('/auth/login', 'AuthController@login')->name('api.auth.login');
+    Route::post('/auth/reset', 'AuthController@reset')->name('api.auth.reset');
+    Route::post('/auth/sms-login', 'AuthController@smsLogin')->name('api.auth.sms-login');
+
+    Route::get('/user/exists', 'UserController@exists')->name('api.user.check-mobile');
+    //验证码
+    Route::get('/user/captcha', 'UserController@captcha')->name('api.user.captcha');
+    Route::post('/user/sms-code', 'UserController@smsCode')->name('api.user.sms-code');
+
+
+
+
+
+});
+
+
