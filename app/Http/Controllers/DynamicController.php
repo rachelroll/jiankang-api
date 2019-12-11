fe525e1ada9c0af0f29930da704ef1035ddcaa72
lixiuye <hai30@163.com>
2019-12-11 10:12:14 +0800

    database setting

---
+++ app/Http/Controllers/DynamicController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DynamicController extends Controller
+{
+    //
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,15 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('nickname')->default('')->comment('昵称');
+            $table->string('avatar')->default('')->comment('头像');
+            $table->string('role')->default('')->comment('角色 0: 不可发布 | 1: 可发布');
+            $table->string('gender')->default('')->comment('性别 0: 男 | 1: 女');
+            $table->string('phone')->default('')->comment('手机号');
+            $table->string('openid')->default('')->comment('openid');
+            $table->string('unionid')->default('')->comment('unionid');
+
             $table->rememberToken();
             $table->timestamps();
         });
